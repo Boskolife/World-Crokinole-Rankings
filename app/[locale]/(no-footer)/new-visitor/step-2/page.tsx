2bf1362675e1ed75d882cd9c6e668312abb52916
Volodymyr Alputov <alputov1998@gmail.com>
2026-02-06 01:45:07 +0200

    fix: fix for build

--- app/[locale]/(no-footer)/new-visitor/step-2/page.tsx
+++ app/[locale]/(no-footer)/new-visitor/step-2/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Suspense } from "react";
 import { HomeNewVisitor } from "@/widgets/home-new-visitor";
 import { Step2 } from "@/widgets/home-new-visitor/Step-2";
 
 export default function Page() {
     return (
         <HomeNewVisitor>
-            <Step2 />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Step2 />
+            </Suspense>
         </HomeNewVisitor>
     );
 }
